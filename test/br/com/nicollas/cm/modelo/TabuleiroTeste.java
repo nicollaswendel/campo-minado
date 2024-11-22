564b1b8fa99228816da376f4f952d2485ec0ecdf
nicollaswendel <nicollaswendel1623@gmail.com>
2024-11-22 18:03:52 -0300

    feat: board test class

--- test/br/com/nicollas/cm/modelo/TabuleiroTeste.java
+++ test/br/com/nicollas/cm/modelo/TabuleiroTeste.java
@@ -1,5 +1,36 @@
 package br.com.nicollas.cm.modelo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TabuleiroTeste {
 
+	private Tabuleiro tabuleiro;
+	
+	@BeforeEach
+	void inciarTabuleiro() {
+		tabuleiro = new Tabuleiro(6, 6, 3);
+	}
+
+	@Test
+	void testeAbrirCoordenada() {
+		tabuleiro.abrir(5, 3);
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		tabuleiro.alternarMarcacao(4, 2);
+	}
+	
+	@Test
+	void testeFimDeJogoGanho() {
+		tabuleiro.objetivoAlcancado();
+		tabuleiro.reiniciar();
+	}
+	
+	@Test
+	void testeImpressaoDasInformacoes() {
+		tabuleiro.toString();
+	}
+	
 }
